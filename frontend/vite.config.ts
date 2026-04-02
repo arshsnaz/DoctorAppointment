f35c392b22e276c064a8f82b929b27c0ff66f126
Abubakar Mohammed Afzal Chanda <122260192+arshsnaz@users.noreply.github.com>
2026-04-03 03:04:34 +0530

    fix: correct base path for vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: '/DoctorAppointment/',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/DoctorAppointment/',
   server: {
     host: "::",
     port: 8080,
